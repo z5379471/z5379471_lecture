e0a0e11776d9edff9a0e8ef43e64b5e39589279e
Yaqi zhou <129933428+z5379471@users.noreply.github.com>
2023-04-25 16:04:43 +1000

    Update lec_pd_series.py

--- lectures/lec_pd_series.py
+++ lectures/lec_pd_series.py
@@ -39,24 +39,24 @@ prices = [
 #   Create a Series instance
 # ----------------------------------------------------------------------------
 # Create a series object
-ser  = '?'
+ser  = pd.Series(data=prices, index=dates)
 #print(ser)
 
 # Select Qantas price on '2020-01-02' ($7.16) using ...
 
 # ... the `prices` list
-prc0  = '?'
+prc0  = prices[dates.index('2020-01-02')]
 #print(prc0)
 
 # ... the `ser` series
-prc1  = '?'
+prc1  = ser['2020-01-02']
 #print(prc1)
 
 # ----------------------------------------------------------------------------
 #   Slicing series
 # ----------------------------------------------------------------------------
 # Unlike dictionaries, you can slice a series
-prcs  = '?'
+prcs  = ser['2020-01-06':'2020-01-10']
 #print(prcs)
 
 # ----------------------------------------------------------------------------
@@ -64,7 +64,7 @@ prcs  = '?'
 # ----------------------------------------------------------------------------
 
 # Use `.array` to get the underlying data array
-ary  = '?'
+ary  = ser.array
 #print(ary)
 
 # Like any instance, you can get its type (i.e., the class used to create the
@@ -104,7 +104,7 @@ the_index  = '?'
 #print(ser)
 
 # This will return 7.04
-x  = '?'
+x  = ser[1000]
 #print(x)
 
 # Compare the following cases:
